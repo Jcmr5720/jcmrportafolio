984f7aa4decdf066bad2da48b6b3b639f25ea151
JCMR <132085703+Jcmr5720@users.noreply.github.com>
2023-06-13 15:22:36 -0500

    commit 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app1'>
+      <div className='app1-1'>
+        <span className='span1App1-1'>Hola mundo xd</span>
+        <span className='span2App1-1'>jeje chau :p</span>
+        <span className='span3App1-1'>muaaaa</span>
+      </div>
+      <div className='app1-2'>
+        <span>Aca no veas</span>
+      </div>
     </div>
   );
 }
